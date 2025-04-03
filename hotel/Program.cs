298a18a0ba523274cf199639a92e2695ebd7a978
Iza <izabela.wasilczuk@infotech.edu.pl>
2025-04-03 13:40:36 +0200

    tak

--- hotel/Program.cs
+++ hotel/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Room pokoj1 = new Room();
+            Room pokoj2 = new Room();
+            Room pokoj3 = new Room();
+            Room pokoj4 = new Room();
+            Room pokoj5 = new Room();
+            Room pokoj6 = new Room();
+            Room pokoj7 = new Room();
+            Room pokoj8 = new Room();
+            Room pokoj9 = new Room();
+            Room pokoj10 = new Room();
+            Room pokoj11 = new Room();
+            Room pokoj12 = new Room();
+        }
+
+    }
+}
+
+class Room
